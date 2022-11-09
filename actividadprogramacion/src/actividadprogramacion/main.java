5df72a77f691ba73622aa5c24ec190203f433ee6
MartiteguiAlejandro <alejandro.marticampa@gmail.com>
2022-11-09 13:55:14 +0100

    primer commit

---
+++ actividadprogramacion/src/actividadprogramacion/main.java
@@ -0,0 +1,51 @@
+package actividadprogramacion;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ventanas/src/ventanas/Miventana.java
@@ -0,0 +1,55 @@
+package ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import java.awt.Button;
+import java.awt.TextField;
+import javax.swing.JTextPane;
+
+public class Miventana {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Miventana window = new Miventana();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Miventana() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		
+	}
+}
